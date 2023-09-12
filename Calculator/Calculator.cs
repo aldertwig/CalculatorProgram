3baafce0de19092a74e7a87c2507f6f90b5babe7
johax aldertwig <carl.johan.ahlqvist@outlook.com>
2023-09-12 14:52:02 +0200

    Add project files.

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorProgram
+{
+    internal class Calculator
+    {
+        public float result { get; set; }
+
+        public Calculator(int startResult)
+        {
+            result = startResult;
+        }
+
+        public void Add(int number1, int number2)
+        {
+            result = number1 + number2;
+        }
+
+        public void Substract(int number1, int number2)
+        {
+            result = number1 - number2;
+        }
+
+        public void Divide(int number1, int number2)
+        {
+            result = (float)number1 / (float)number2;
+        }
+
+        public void Multiply(int number1, int number2)
+        {
+            result = number1 * number2;
+        }
+
+        public void Add(int number1)
+        {
+            result += number1;
+        }
+
+        public void Substract(int number1)
+        {
+            result -= number1;
+        }
+
+        public void Divide(int number1)
+        {
+            result /= (float)number1;
+        }
+
+        public void Multiply(int number1)
+        {
+            result *= number1;
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace CalculatorProgram
+{
+    internal class Program
+    {
+        enum Operator
+        {
+            Addition = '+',
+            Substraction = '-',
+            Division = '/',
+            Multiplication = '*'
+        }
+
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator(startResult: 0);
+
+            bool runCalculator = true;
+            Console.WriteLine($"Operators: {(char)Operator.Addition} " +
+                              $"{(char)Operator.Substraction} " +
+                              $"{(char)Operator.Division} " +
+                              $"{(char)Operator.Multiplication} " +
+                              "Usage: NUMBER OPERATOR NUMBER (ENTER)");
+            while (runCalculator)
+            {
+
+                string inputString = Console.ReadLine();
+                if (inputString.Length > 0)
+                {
+                    char calcOperator = ' ';
+                    int number1 = 0;
+                    int number2 = 0;
+                    string[] inputSplitted = inputString.Split(' ');
+
+                    if (inputSplitted.Length == 3)
+                    {
+                        calcOperator = Convert.ToChar(inputSplitted[1]);
+                        number1 = Convert.ToInt32(inputSplitted[0]);
+                        number2 = Convert.ToInt32(inputSplitted[2]);
+                    }
+                    else if (inputSplitted.Length == 2)
+                    {
+                        calcOperator = Convert.ToChar(inputSplitted[0]);
+                        number1 = Convert.ToInt32(inputSplitted[1]);
+                    }
+
+                    switch (calcOperator)
+                    {
+                        case (char)Operator.Addition:
+                            if (inputSplitted.Length == 3)
+                            {
+                                calc.Add(number1, number2);
+                            }
+                            else if (inputSplitted.Length == 2)
+                            {
+                                calc.Add(number1);
+                            }
+                            Console.WriteLine(calc.result);
+                            break;
+                        case (char)Operator.Substraction:
+                            if (inputSplitted.Length == 3)
+                            {
+                                calc.Substract(number1, number2);
+                            }
+                            else if (inputSplitted.Length == 2)
+                            {
+                                calc.Substract(number1);
+                            }
+                            Console.WriteLine(calc.result);
+                            break;
+                        case (char)Operator.Division:
+                            if (inputSplitted.Length == 3)
+                            {
+                                calc.Divide(number1, number2);
+                            }
+                            else if (inputSplitted.Length == 2)
+                            {
+                                calc.Divide(number1);
+                            }
+                            Console.WriteLine(calc.result);
+                            break;
+                        case (char)Operator.Multiplication:
+                            if (inputSplitted.Length == 3)
+                            {
+                                calc.Multiply(number1, number2);
+                            }
+                            else if (inputSplitted.Length == 2)
+                            {
+                                calc.Multiply(number1);
+                            }
+                            Console.WriteLine(calc.result);
+                            break;
+                        default:
+                            Console.Write($"Use operators: {(char)Operator.Addition} " +
+                                            $"{(char)Operator.Substraction} " +
+                                            $"{(char)Operator.Division} " +
+                                            $"{(char)Operator.Multiplication}");
+                            Console.ReadLine();
+                            break;
+                    }
+                }
+                else
+                {
+                    runCalculator = false;
+                }
+            }
+        }
+    }
+}
